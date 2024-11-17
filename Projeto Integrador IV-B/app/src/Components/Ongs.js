e9f0260720d3157c29fa37be03870e19f06ab20e
brunomarques97 <brunomarques266@gmail.com>
2024-11-16 21:33:41 -0300

    Criação da tela da ongs

--- Projeto Integrador IV-B/app/src/Components/Ongs.js
+++ Projeto Integrador IV-B/app/src/Components/Ongs.js
@@ -1,10 +1,30 @@
 import './Ongs.css';
 
+import React from 'react';
+
+import data from '../data/pets.json'
+
 const Ongs=()=>{  
 
   return (
-    <section className='main'>
+    <section className='main container'>
       <h1>Ongs</h1>
+      <table>
+      <thead>
+        <tr>
+          <th className='tabela'>Address</th>
+          <th className='tabela'>Contact</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index}>
+            <td className='tabela'>Rua:{item.rua} | Cidade:{item.cidade} | Estado:{item.estado} | Pais:{item.pais}</td>
+            <td className='tabela'>Email:{item.email} | Numero:{item.telefone}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
     </section>
   );
 }
