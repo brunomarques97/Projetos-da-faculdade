0d54adad07c9ce14e32ed952d6157f7c980b40f1
brunomarques97 <brunomarques266@gmail.com>
2025-04-19 06:27:20 -0300

    Alterei a pagina das ongs

--- Projeto Integrador V-A/app/src/Components/Ongs.js
+++ Projeto Integrador V-A/app/src/Components/Ongs.js
@@ -1,10 +1,18 @@
 import './Ongs.css';
 
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 
-import data from '../data/pets.json'
 
-const Ongs=()=>{  
+const Ongs=()=>{
+
+   const [data, setData] = useState([]);
+  
+    useEffect(() => {
+        fetch('http://localhost/pets/dados.php?acao=listar&tabela=ongs')
+            .then(response => response.json())
+            .then(data => setData(data))
+            .catch(error => console.error('Erro:', error));
+    }, []);
 
   return (
     <section className='main container'>
@@ -14,16 +22,17 @@ const Ongs=()=>{
       <table>
       <thead>
         <tr>
-          <th className='tabela'>Address</th>
-          <th className='tabela'>Contact</th>
+        <th className='tabela'>Nome</th>
+          <th className='tabela'>Endereço</th>
+          <th className='tabela'>Contato</th>
         </tr>
       </thead>
       <tbody>
         {data.map((item, index) => (
           <tr key={index}>
-            <td className='tabela'>Road:{item.rua} | City:{item.cidade} | State:{item.estado} | Country:{item.pais}</td>
-            <td className='tabela'>Email:{item.email} | 
-            Number:{item.telefone}</td>
+            <td className='tabela'>Nome: {item.Nome}</td>
+            <td className='tabela'>Rua:{item.Rua} | Cidade:{item.Cidade} | Estado:{item.Estado} | Pais:{item.Pais}</td>
+            <td className='tabela'>Email:{item.Email} | Numero:{item.Numero}</td>
           </tr>
         ))}
       </tbody>
